758b5be223603a5fb7fb487626649aa87191b846
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-07-04 13:45:47 +0100

    feat(04-extra): use prop getters to compose props

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -7,18 +7,39 @@ import {Switch} from '../switch'
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  const togglerProps = {'aria-pressed': on, onClick: toggle}
+  function getTogglerProps({onClick, ...props} = {}) {
+    return {
+      'aria-pressed': on,
+      onClick: () => {
+        onClick && onClick()
+        toggle()
+      },
+      on,
+      toggle,
+      ...props,
+    }
+  }
 
-  return {on, toggle, togglerProps}
+  return {
+    on,
+    toggle,
+    getTogglerProps,
+  }
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
